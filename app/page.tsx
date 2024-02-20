1dafe97acd5dd418cdf910bc3aaf8fbe1be2f6aa
Piyush <sainipiyush8860@gmail.com>
2024-02-20 21:29:36 +0530

    Keyboard import setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 "use client"
 import Spline from '@splinetool/react-spline';
+import { Fragment } from 'react';
 
 export default function App() {
   return (
-    <div className='w-screen h-screen '
-    >
-       <Spline scene="https://prod.spline.design/u8kMnjIBlBR9eCSO/scene.splinecode" />
-    </div>
+    <Fragment>
+      <div className='w-screen h-screen flex border border-white '
+      >
+          <Spline className='border  w-[40%] border-red-300 cursor-grab ' 
+           scene="https://prod.spline.design/u8kMnjIBlBR9eCSO/scene.splinecode" />
+          <div className='w-[60%]'>
+            hi
+          </div>
+      </div>
+        <div className='100vh' >
+
+        </div>
+    </Fragment>
+
   );
 }
